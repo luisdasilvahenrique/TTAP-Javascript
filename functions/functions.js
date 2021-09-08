d79e9d72d4547c317ff5441a2dfb36364102f07f
Luís Henrique <luis.silva@alu.ufc.br>
2021-09-08 13:52:07 -0300

    functions

---
+++ functions/functions.js
@@ -0,0 +1,14 @@
+// criar um app de frases motivacionais 
+
+// declaration = declaração da função
+// function statement
+function createPhrases() {
+    console.log('Estudar é emuito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+// executar a função
+// execute, run, call, invoke
+
+createPhrases()
