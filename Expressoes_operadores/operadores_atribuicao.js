915266908f83e5f9b864c8ffb0af52763d61b884
Luís Henrique <luis.silva@alu.ufc.br>
2021-09-14 11:56:57 -0300

    operadores de expresão

---
+++ Expressoes_operadores/operadores_atribuicao.js
@@ -0,0 +1,28 @@
+// Operadores de atribuição (Assignment)
+let x
+//Assignment
+x = 1
+console.log(x)
+
+// addition assignment 
+
+x += 2
+console.log(x)
+
+// subtraction assignment
+ x -= 1
+console.log(x)
+
+// multiplication assignmet 
+x *= 2
+console.log(x)
+
+// division assignmet 
+x /= 2
+console.log(x)
+
+// remainder assginment
+x **= 4
+console.log(x)
+
+// exponencial 
