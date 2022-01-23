5911a9d3e91a35101dd693898bb64ef8facb8042
Luís Henrique <61327251+luisdasilvahenrique@users.noreply.github.com>
2022-01-22 23:04:47 -0300

    Create function console.table
    Apresenta em tela um array de dados em uma tabela. Como se fosse banco de dados

---
+++ manipulating_data/console.table.js
@@ -0,0 +1,7 @@
+const cadastro = [
+   { name: "Luís Henrique", age: 21, andress: "Francisco Castelo de Castro" }, 
+   { name: "Luiza Rodrigues", age: 16, andress: "Vila Betânia" },
+   { name: "Gabriel Romão", age: 20, andress: "Vila Salete" },
+]
+
+console.table(cadastro)
